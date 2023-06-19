37bb33e829f664f4b9eac8f20e2fc13a6ceebb15
KeremZeybek <recepkeremzeybek@gmail.com>
2023-06-19 20:19:30 +0300

    selectors

---
+++ crypto.js
@@ -0,0 +1,9 @@
+// api key coinranking5787f1b20234b3e445601abdfdba9358393fbf304707a9ff
+
+const form = document.querySelector(".top-banner form");
+
+const input = document.querySelector(".top-banner input");
+
+const msgSpan = document.querySelector(".container .msg");
+
+const coinList = document.querySelector(".ajax-section .container .coins");
