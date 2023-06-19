7fafbfb3c16c3d28ae9b92f39242cfef3365981f
KeremZeybek <recepkeremzeybek@gmail.com>
2023-06-19 20:39:02 +0300

    apiKey

--- crypto.js
+++ crypto.js
@@ -8,11 +8,22 @@ const msgSpan = document.querySelector(".container .msg");
 
 const coinList = document.querySelector(".ajax-section .container .coins");
 
+//*localstoreage
+
+localStorage.setItem(
+  "apiKey",
+  EncryptStringAES(
+    "coinranking5787f1b20234b3e445601abdfdba9358393fbf304707a9ff"
+  )
+);
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   getCoinDataFromApi();
+  form.reset();
 });
 
 const getCoinDataFromApi = () => {
-  alert("Get Coin Data!");
+  const apiKey = window.localStorage.getItem("apiKey");
+  console.log(apiKey)
 };
